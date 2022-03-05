874cd620040932cb20e4907bb23de64d8d235ddf
XuWu <ixw1991@126.com>
2022-03-05 11:36:45 +0800

    create commitlint.config.js [no ci]

---
+++ commitlint.config.js
@@ -0,0 +1,29 @@
+const Configuration = {
+  /*
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   * Referenced packages must be installed
+   */
+  extends: ['@commitlint/config-conventional'],
+  
+  rules: {
+			2,
+			'always',
+			[
+				'build',
+				'chore',
+				'ci',
+				'docs',
+        'doc',
+				'feat',
+				'fix',
+				'perf',
+				'refactor',
+				'revert',
+				'style',
+				'test',
+			],
+  },
+ 
+};
+
+module.exports = Configuration;
